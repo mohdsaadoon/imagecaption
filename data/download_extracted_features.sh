7f0b028d3543b703531fdf1cf99893f37ef3acc6
Mohammad <61922543+sadoun92@users.noreply.github.com>
2022-05-11 09:47:05 +1000

    Delete data directory

--- data/download_extracted_features.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "download text from https://raw.githubusercontent.com/mutazag/mdsi/master/download/extracted_features.zip"
-curl -L " https://raw.githubusercontent.com/mutazag/mdsi/master/download/extracted_features.zip" --output extracted_features.zip
